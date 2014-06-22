8f9f608ec38f21df78df0cf8fc37a81fc43c5dda
Daniel Blanco Martin <daniel@blancomartin.cl>
2014-06-21 20:05:44 -0400

    Change name to directory for easiest typing

--- In Your WebSite/include/forms/logout.php
+++
@@ -1,8 +0,0 @@
-<?php
-	session_start();
-	unset($_SESSION['user_arr']);
-	unset($_SESSION['auth']);
-	
-	
-	header('location:/');
-?>
--- In Your WebSite/include/forms/recuperarlogin.php
+++
@@ -1,6 +0,0 @@
-<?php
-	session_start();
-	$_SESSION['recuperarlogin_view'] = 1;
-	
-	header('location:/');
-?>
